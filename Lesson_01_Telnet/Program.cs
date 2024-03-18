6defac9ed1a8a0f1a08b2ee372a3a60acd6074af
nitesw <apedos2007@gmail.com>
2024-03-18 22:55:29 +0200

    Add project files.

---
+++ Lesson_01_Telnet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lesson_01_Telnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Any;
+            IPEndPoint port = new IPEndPoint(ipAddress, 8080);
+
+            socketListener.Bind(port);
+            socketListener.Listen(5);
+
+            Console.WriteLine("Server has been started...");
+
+            Socket client = socketListener.Accept();
+            Console.WriteLine($"New client has been connected... {client.ToString()} IP: {client.RemoteEndPoint.ToString()}");
+
+            byte[] buffer = new byte[1024];
+            int numberOfReceivedBytes = 0;
+
+            while (true)
+            {
+                numberOfReceivedBytes = client.Receive(buffer);
+                Console.WriteLine($"Number of received bytes: {numberOfReceivedBytes}");
+                Console.WriteLine($"Data sent: {buffer}");
+
+                string receivedData = Encoding.ASCII.GetString(buffer, 0, numberOfReceivedBytes);
+                Console.WriteLine($"Command from client {receivedData}");
+                client.Send(buffer);
+
+                if(receivedData == "0")
+                {
+                    break;
+                }
+
+                Array.Clear(buffer, 0, buffer.Length);
+                numberOfReceivedBytes = 0;
+            }
+        }
+    }
+}
---
+++ Lesson_01_Telnet_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
+
+namespace Lesson_01_Telnet_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Any;
+            IPEndPoint port = new IPEndPoint(ipAddress, 8080);
+
+            socketListener.Bind(port);
+            socketListener.Listen(5);
+
+            Console.WriteLine("Server has been started...");
+
+            Socket client = socketListener.Accept();
+            Console.WriteLine($"New client has been connected... {client.ToString()} IP: {client.RemoteEndPoint.ToString()}");
+
+            byte[] buffer = new byte[1024];
+            int numberOfReceivedBytes = 0;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (true)
+            {
+                numberOfReceivedBytes = client.Receive(buffer);
+                Console.WriteLine($"Number of received bytes: {numberOfReceivedBytes}");
+                Console.WriteLine($"Data sent: {buffer}");
+
+                string receivedData = Encoding.ASCII.GetString(buffer, 0, numberOfReceivedBytes).Trim();
+                receivedData = receivedData.Replace("\r", "").Replace("\n", "");
+                Console.WriteLine($"Command from client {receivedData}");
+                client.Send(buffer);
+
+                if (receivedData != "0")
+                {
+                    stringBuilder.Append(receivedData);
+                }
+                else if (receivedData == "0")
+                {
+                    break;
+                }
+
+                Array.Clear(buffer, 0, buffer.Length);
+                numberOfReceivedBytes = 0;
+            }
+            try
+            {
+                stringBuilder.Append(".exe");
+                string program = stringBuilder.ToString();
+                Process process = Process.Start(program);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
